5c6e25c1d63a379ff67c2f22f9c8e8bfcfbb07ca
ToprakArslann <toprakarslan0139@gmail.com>
2025-06-01 01:37:39 +0300

    Added Login Page

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,29 +1,44 @@
+import React, { useState } from "react";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import MainPage from "./components/MainPage";
 import SideMenu from "./components/SideMenu";
 import SongInfo from "./components/SongInfo";
+import LoginPage from "./components/LoginPage";
+
 
 export default function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
+
   return (
     <>
-      <div className="bg-black h-screen w-screen flex flex-col ">
-        <div className="h-14 flex-shrink-0">
-          <Header />
+      {!loggedIn &&
+        <div className="w-screen h-screen flex ">
+          <LoginPage/>
         </div>
-        <div className="flex-1 flex m-2 min-h-0">
-          <div className="flex-shrink-0">
-            <SideMenu />
+      }
+      {loggedIn && 
+        <div className="bg-black h-screen w-screen flex flex-col ">
+          <div className="h-14 flex-shrink-0">
+            <Header />
           </div>
-          <div className="flex-1 flex min-h-0">
-            <MainPage />
-            <SongInfo />
+          <div className="flex-1 flex m-2 gap-2 min-h-0">
+            <div className="flex-shrink-0 bg-amber-200">
+              <SideMenu />
+            </div>
+            <div className="flex-1 flex min-h-0 bg-amber-300">
+              <MainPage />
+            </div>
+            <div className="flex-1 flex min-h-0 bg-amber-700">
+              <SongInfo />
+            </div>
+          </div>
+          <div className="h-[78px] flex-shrink-0 ">
+            <Footer />
           </div>
         </div>
-        <div className="h-[78px] flex-shrink-0 ">
-          <Footer />
-        </div>
-      </div>
+      }
+      
     </> 
   )
 }
